e55ca55c84a972c9614ccd4d2d590f227ea0a10e
furiousbloom <yoonslover@gmail.com>
2021-05-23 22:44:35 +0900

    minor commit

--- HELLOPYTHON/day14/mymnist02.py
+++ HELLOPYTHON/day14/mymnist02.py
@@ -1,26 +1,26 @@
 import tensorflow as tf
 
-# 1. MNIST 데이터넷 임포트
+# 1. MNIST �뜲�씠�꽣�꽬 �엫�룷�듃
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# 2. 데이터 전처리
+# 2. �뜲�씠�꽣 �쟾泥섎━
 x_train, x_test = x_train/255.0, x_test/255.0
 
-# 3. 모델 구성
+# 3. 紐⑤뜽 援ъ꽦
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(512, activation=tf.nn.relu),
   tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
-# 4. 모델 컴파일
+# 4. 紐⑤뜽 而댄뙆�씪
 model.compile(optimizer='adam',
             loss='sparse_categorical_crossentropy',
             metrics=['accuracy'])
 
-# 5. 모델 훈련
-model.fit(x_train, y_train, epochs=100)
+# 5. 紐⑤뜽 �썕�젴
+model.fit(x_train, y_train, epochs=20)
 
-# 6. 모델 저장
+# 6. 紐⑤뜽 ���옣
 model.save('model1.h5')
